74831640254bb0fd1ffce0fc4d1e89ded1fbfa22
Phillip Langley <philliplangley@champmbplangley.lan>
2023-01-12 12:06:44 -0600

    adding providers

--- variables.tf
+++ variables.tf
@@ -1,1 +1,17 @@
+variable "keycloak_client_secret" {
+  description = "https://registry.terraform.io/providers/mrparkers/keycloak/latest/docs#username"
+  default     = ""
+  type        = string
+}
 
+variable "keycloak_url" {
+  description = "https://registry.terraform.io/providers/mrparkers/keycloak/latest/docs#username"
+  default     = ""
+  type        = string
+}
+
+variable "keycloak_client_id" {
+  description = "https://registry.terraform.io/providers/mrparkers/keycloak/latest/docs#client_id"
+  default     = "terraform-client"
+  type        = string
+}
