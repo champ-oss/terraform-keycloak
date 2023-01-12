05aa878fb27a8b1a45807411a07c095372555ff5
Phillip Langley <philliplangley@champmbplangley.lan>
2023-01-12 16:30:37 -0600

    testing changes

--- main.tf
+++
@@ -1,6 +0,0 @@
-provider "keycloak" {
-  client_id      = "terraform-client"
-  client_secret  = "884e0f95-0f42-4a63-9b1f-94274655669e"
-  url            = "http://localhost:8080"
-  client_timeout = 3600
-}
