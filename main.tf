26acee54943be499ea98329b4ecbac8d73e1b82b
Phillip Langley <philliplangley@champmbplangley.lan>
2023-01-12 11:49:36 -0600

    updating resource

--- main.tf
+++
@@ -1,5 +0,0 @@
-provider "keycloak" {
-  client_id     = var.keycloak_client_id
-  client_secret = var.keycloak_client_secret
-  url           = var.keycloak_url
-}
--- variables.tf
+++ variables.tf
@@ -1,17 +1,1 @@
-variable "keycloak_client_secret" {
-  description = "https://registry.terraform.io/providers/mrparkers/keycloak/latest/docs#username"
-  default     = ""
-  type        = string
-}
 
-variable "keycloak_url" {
-  description = "https://registry.terraform.io/providers/mrparkers/keycloak/latest/docs#username"
-  default     = ""
-  type        = string
-}
-
-variable "keycloak_client_id" {
-  description = "https://registry.terraform.io/providers/mrparkers/keycloak/latest/docs#client_id"
-  default     = "terraform-client"
-  type        = string
-}
